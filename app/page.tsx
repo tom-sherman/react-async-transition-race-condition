fb155e11db24ad96521026a338f5d3574680a283
Tom Sherman <the.tomsherman@gmail.com>
2024-04-12 00:05:39 +0100

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,45 @@
-/** Add your relevant code here for the issue to reproduce */
+"use client";
+
+import { useState, useTransition } from "react";
+
+let first = true;
+
 export default function Home() {
-  return null;
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState<string[]>([]);
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => {
+          const newSearch = e.target.value;
+          setSearch(newSearch);
+          startTransition(async () => {
+            if (first) {
+              first = false;
+              await new Promise((resolve) => setTimeout(resolve, 500));
+            }
+            setResults(await getResults(newSearch));
+          });
+        }}
+      />
+      {isPending ? "Loading..." : null}
+      <ul>
+        {results.map((result) => (
+          <li key={result}>{result}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+async function getResults(query: string) {
+  await new Promise((resolve) => setTimeout(resolve, 250));
+
+  return ["a", "aa", "ab", "b", "bc"].filter((result) =>
+    result.includes(query)
+  );
 }
